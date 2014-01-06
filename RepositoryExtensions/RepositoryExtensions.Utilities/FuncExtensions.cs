1906f4ed3279895b662a2a874ae2ef81901da41d
Alexander Kahoun <alexkahoun@gmail.com>
2014-01-05 22:02:00 -0700

    Added a utility module for common code

---
+++ RepositoryExtensions/RepositoryExtensions.Utilities/FuncExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace RepositoryExtensions.Utilities
+{
+    public static class FuncExtensions
+    {
+        public static Expression<Func<TSource, TOut>> Compose<TSource, TOut, TDestination>(
+            this Expression<Func<TDestination, TOut>> outer,
+            Expression<Func<TSource, TDestination>> inner)
+        {
+            return Expression.Lambda<Func<TSource, TOut>>(
+                ParameterReplacer.Replace(outer.Body, outer.Parameters[0], inner.Body),
+                inner.Parameters[0]);
+        }
+    }
+}
